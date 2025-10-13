57e5d1aba5b5542effe0a09de1b220bcca97ca29
Eduardo-xX <eduardo.nascimento@sptech.school>
2025-10-13 12:39:31 -0300

    Alterando o Header (deixando responsivel)

---
+++ Site/menuDropdown.js
@@ -0,0 +1,9 @@
+function buttonMenu () {
+    var menuDropdown = document.getElementById('menu_dropdown')
+    
+    if (menuDropdown.style.display == 'none') {
+        menuDropdown.style.display = 'block'
+    } else {
+        menuDropdown.style.display = 'none'
+    }
+}
