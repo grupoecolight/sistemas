618909944fc020531aeedf4519de622f8b526129
Eduardo-xX <eduardo.nascimento@sptech.school>
2025-10-28 23:02:15 -0300

    Alterando o print do código do arduino recebido do professor, e também adicionado alguns comentários explicando algumas passos do código

--- Código do Arduino/codigoprofessor/codigoprofessor.ino
+++ Código do Arduino/codigoprofessor/codigoprofessor.ino
@@ -1,10 +1,11 @@
-int ldr_pin = A0;
+int ldr_pin = A0; // pino analógico onde o LDR está ligado
 
-int ldr_read = 0;
+int ldr_read = 0; // variável para armazenar a leitura
 
-float vin = 5.00;
+float vin = 5.00; // tensão de alimentação do circuito (5V do Arduino)
 
-float valor_ADC = 0.00488758, r_ohms = 10000;
+float valor_ADC = 0.00488758; // valor de 1 unidade do conversor ADC (~5V/1023)
+float r_ohms = 10000; // resistor fixo de 10kΩ
 
  
 
@@ -26,58 +27,59 @@ void loop() {
 
   else{
 
-    ldr_read = analogRead(ldr_pin);
+    ldr_read = analogRead(ldr_pin); // converte a tensão entre 0 e 5V para um número de 0 a 1023.
 
-    float vout = valor_ADC * ldr_read;
+    float vout = valor_ADC * ldr_read; // converte o valor ADC/lido para volts
 
-    float res_ldr = (r_ohms * (vin - vout))/vout;
+    float res_ldr = (r_ohms * (vin - vout))/vout; // resistência atual do LDR
 
-    float lux = 500/(res_ldr/1000);
+    float lux = 500/(res_ldr/1000); // estima a iluminação em lux
 
  
+    Serial.println(lux + ";"); // printa o valor em lux para ser recebido na API
 
-    if(ldr_read > 750){
+    delay(5000); // Delay de 5 segundos
 
-      Serial.print(ldr_read);
+    // if(ldr_read > 750){
 
-      Serial.print(" Vout: ");
+    //   Serial.print(ldr_read);
 
-      Serial.print(vout);
+    //   Serial.print(" Vout: ");
 
-      Serial.print(" R_ldr: ");
+    //   Serial.print(vout);
 
-      Serial.print(res_ldr);
+    //   Serial.print(" R_ldr: ");
 
-      Serial.print(" Lux: ");
+    //   Serial.print(res_ldr);
 
-      Serial.print(lux);
+    //   Serial.print(" Lux: ");
 
-      Serial.println(" Claro");
+    //   Serial.print(lux);
 
-    }
+    //   Serial.println(" Claro");
 
-    else{
+    // }
 
-      Serial.print(ldr_read);
+    // else{
 
-      Serial.print(" Vout: ");
+    //   Serial.print(ldr_read);
 
-      Serial.print(vout);
+    //   Serial.print(" Vout: ");
 
-      Serial.print(" R_ldr: ");
+    //   Serial.print(vout);
 
-      Serial.print(res_ldr);
+    //   Serial.print(" R_ldr: ");
 
-      Serial.print(" Lux: ");
+    //   Serial.print(res_ldr);
 
-      Serial.print(lux);
+    //   Serial.print(" Lux: ");
 
-      Serial.println(" Escuro");
+    //   Serial.print(lux);
 
-    }
+    //   Serial.println(" Escuro");
+
+    // }
 
   }
-
-  delay(2000);
 
 }
