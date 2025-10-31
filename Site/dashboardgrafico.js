abe570b34621dcaaa7f0aac7424db6ea83e8bbf4
Eduardo-xX <eduardo.nascimento@sptech.school>
2025-10-31 00:43:24 -0300

    Atualizações no site

--- Site/dashboardgrafico.js
+++ Site/dashboardgrafico.js
@@ -8,7 +8,7 @@ data: {
     labels: ['002-NOR', '012-NOR', '022-NOR', '032-NOR', '042-NOR', '052-NOR', '062-NOR', '072-NOR', '082-NOR', '092-NOR'],
     datasets: [{
         label: '18:00:00 PM',
-        data: [19, 20, 21, 20, 19, 17, 18, 18, 17, 16],
+        data: [9, 10, 11, 10, 9, 7, 8, 8, 7, 6],
         borderWidth: 5,
         backgroundColor: '#637CEF',
         color: '#637CEF',
@@ -31,6 +31,10 @@ options: {
             beginAtZero: true,
             min: 0,
             max: 60,
+            title: {
+                display: true,
+                text: '(%)'
+            },
             ticks: {
                 stepSize: 10
             }
@@ -45,7 +49,7 @@ data: {
     labels: ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30'],
     datasets: [{
     label: '13/10/2025',
-    data: [41, 43, 45, 47, 49, 53, 51, 44, 31, 33],
+    data: [21, 23, 17, 16, 24, 29, 26, 31, 35, 41],
     borderWidth: 5,
     backgroundColor: '#637CEF',
     color: '#637CEF',
@@ -68,6 +72,10 @@ options: {
             beginAtZero: true,
             min: 0,
             max: 60,
+            title: {
+                display: true,
+                text: '(%)'
+            },
             ticks: {
                 stepSize: 10
             }
--- Site/formEmpresa.js
+++ Site/formEmpresa.js
@@ -2,6 +2,7 @@ function buttonSubmitForm () {
     var mensagem = document.getElementById('textarea_mensagem').value;
     var nomeEmpresa = document.getElementById('input_nomeEmpresa').value;
     var email = document.getElementById('input_emailEmpresa').value;
+    var divEmail = document.getElementById('div_mensagemEmail').value
     var nomeRepresentante = document.getElementById('input_nomeRepresentante').value;
     var tellContato = document.getElementById('input_telefoneContato').value;
     var cont = 0
@@ -14,6 +15,17 @@ function buttonSubmitForm () {
         div_mensagemMensagem.innerHTML = `Preencha todos os campos para continuar`
     } else if (cont > 500){
         div_mensagemMensagem.innerHTML = `Erro! A mensagem ultrapassou os 500 caracters`
+    } else if (email.length > 255) { // Não pode ter mais de 255 caracteres
+        div_mensagemMensagem.innerHTML = ''
+        div_mensagemEmail.innerHTML += `Não pode ter mais de 255 caracteres`
+    } else if (email.endsWith('.')) { // Não pode terminar com ponto
+        div_mensagemMensagem.innerHTML = ''
+        div_mensagemEmail.innerHTML += `Não pode terminar com ponto`
+    } else if (email.includes('@') == false) { // Tem que ter pelo menos 1 '@'
+        div_mensagemMensagem.innerHTML = ''
+        div_mensagemEmail.innerHTML += `Tem que ter pelo menos 1 '@'`
+    } else if (tellContato.length < 15) {
+        div_mensagemMensagem.innerHTML = 'Preencha o Campo Telefone corretamente.'
     } else {
         div_mensagemMensagem.innerHTML = ``
         alert(`Mensagem enviada`)
@@ -25,3 +37,46 @@ function buttonSubmitForm () {
     }
 }
 
+function valEmail() {
+    var email = document.getElementById('input_emailEmpresa').value
+
+    var tamanho = email.length - 1
+    
+    if (email.includes('@')) { // Não pode ter mais de 1 '@'
+        email = email.replace('@', '*')
+        if (email.includes('@')) {
+            div_mensagemEmail.innerHTML = 'Apenas 1 "@" é permitido'
+        } else {
+            div_mensagemEmail.innerHTML = ''
+        }    
+    } else {
+        div_mensagemEmail.innerHTML = ''
+    }    
+
+    if (email[0] == '.' || (email[tamanho] == '.' && email[(tamanho - 1)] == '.')) { // Não deixa começar com ponto e nem ter 2 pontos seguidos
+        input_emailEmpresa.value = input_emailEmpresa.value.slice(0, -1)
+    }
+    
+    if (email[tamanho] == ' ' || email[tamanho] == ',' || email[tamanho] == ':' || email[tamanho] == ';') { // Não pode usar 'espaço', 'vírgula', ':', ';'
+        input_emailEmpresa.value = input_emailEmpresa.value.slice(0, -1)
+    }
+}
+
+function valTel() {
+    var tel = document.getElementById('input_telefoneContato').value
+    var tamanho = tel.length - 1
+
+    if (!isNaN(tel[tamanho]) && !(tel[tamanho] == ' ')) { // Deixa digitar apenas números
+        if (tamanho == 2 && tel[0] != '(') { // Coloca os parenteses automatico a partir do terceiro número colocado
+            input_telefoneContato.value = `(${tel[0]}${tel[1]}) ${input_telefoneContato.value.slice(2, (tamanho + 1))}`
+        } else if (tamanho == 10) { // Coloca o traço automatico
+            input_telefoneContato.value = input_telefoneContato.value.slice(0, -1) + '-' + input_telefoneContato.value[tamanho]
+        }
+    } else { // Apaga tudo que não for número
+        input_telefoneContato.value = input_telefoneContato.value.slice(0, -1)
+    }
+    
+    if (tamanho > 14) { // Não deixa colocar mais que 14 caracteres
+        input_telefoneContato.value = input_telefoneContato.value.slice(0, -1)
+    }
+}
